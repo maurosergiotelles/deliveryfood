c4341341706983ab6dfa9e0f1e0445d93ea09baa
Mauro Telles <etasks2001@gmail.com>
2024-07-23 19:30:14 -0300

    profile teste completo

--- src/main/java/com/deliveryfood/test/EmailClienteEnviarEvent.java
+++ src/main/java/com/deliveryfood/test/EmailClienteEnviarEvent.java
@@ -3,12 +3,10 @@ package com.deliveryfood.test;
 import org.springframework.context.annotation.Profile;
 
 @Profile("test")
-public class EmailClienteEnviarEvent  {
-
+public class EmailClienteEnviarEvent {
 
 	public EmailClienteEnviarEvent(String mensagem) {
 		System.out.println("EVENTO: " + mensagem);
 	}
-	
-	
+
 }
--- src/main/java/com/deliveryfood/test/EmailUtil.java
+++ src/main/java/com/deliveryfood/test/EmailUtil.java
@@ -1,6 +1,7 @@
 package com.deliveryfood.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -9,19 +10,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailUtil {
 
+	
+	@Value("${host.email}")
+	private String hostEmail;
+	
+	
+	
 	@Autowired
 	private ApplicationEventPublisher eventPublisher;
-	
+
 	public EmailUtil() {
 		System.out.println("Inicializado.");
 	}
 
+	
+	
+	
 	public void enviar() {
 
-		
-		System.out.println("Enviando emailfsdafda");
-		
+		System.out.println("Enviando emailfsdafda pelo host:>>>" + hostEmail);
+
 		eventPublisher.publishEvent(new EmailClienteEnviarEvent("Prezado cliente"));
-		
+
 	}
 }
