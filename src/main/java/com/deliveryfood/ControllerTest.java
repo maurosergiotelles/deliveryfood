2027cbbf78130409eb0aafdd2ffee1c237132e27
Mauro Telles <etasks2001@gmail.com>
2024-07-23 15:23:05 -0300

    controller de teste alterado

--- src/main/java/com/deliveryfood/ControllerTest.java
+++ src/main/java/com/deliveryfood/ControllerTest.java
@@ -1,11 +1,13 @@
 package com.deliveryfood;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/base2")
 public class ControllerTest {
 
 	@GetMapping("/test")
